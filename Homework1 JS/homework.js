0a94f9f71c546770fcbc6991b213c29fa88e1729
StefanGligorov <119980150+StefanGligorov@users.noreply.github.com>
2022-12-07 00:08:50 +0100

    Add files via upload

---
+++ Homework1 JS/homework.js
@@ -0,0 +1,11 @@
+let userInput1 = 119.95;
+let userPercentage = 5;
+let userInput2 = 30;
+
+let multiply = userInput1 * userInput2;
+console.log(multiply);
+
+let taxSol = (5 / 100) * multiply;
+console.log(taxSol);
+
+
